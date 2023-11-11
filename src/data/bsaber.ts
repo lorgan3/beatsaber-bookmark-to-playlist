d0e40f8f209e81ea8d1c743d21bd1d21b5a4ee16
Lennert Claeys <claeyslennert@gmail.com>
2023-11-11 16:53:05 +0100

    Basic error handling in case something goes wrong

--- src/data/bsaber.ts
+++ src/data/bsaber.ts
@@ -9,6 +9,8 @@ export interface Params {
 const BSABER_API_URL = "https://bsaber.com/wp-json/bsaber-api/songs";
 const PROXY_URL = "https://corsproxy.io/";
 
+const MAX_RETRIES = 3;
+
 export const fetchSongs = async ({ bookmarkedBy, amount }: Params) => {
   const url = new URL(BSABER_API_URL);
   url.searchParams.set("bookmarked_by", bookmarkedBy);
@@ -16,10 +18,28 @@ export const fetchSongs = async ({ bookmarkedBy, amount }: Params) => {
   const songs: Song[] = [];
 
   let page = 1;
+  let retries = 0;
   while (!amount || songs.length < amount) {
     url.searchParams.set("page", page.toString());
 
-    const response = await fetch(`${PROXY_URL}?${url.toString()}`);
+    let response;
+    try {
+      response = await fetch(
+        `${PROXY_URL}?${encodeURIComponent(url.toString())}`
+      );
+
+      if (!response.ok) {
+        throw new Error(await response.text());
+      }
+    } catch (error) {
+      retries++;
+      if (retries < MAX_RETRIES) {
+        continue;
+      }
+
+      throw new Error("Failed to fetch bookmarks", { cause: error });
+    }
+
     const json = camelcaseKeys(await response.json()) as {
       songs: Song[];
       nextPage: number | null;
