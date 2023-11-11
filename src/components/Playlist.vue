08fc9bfcb10960c3a8dcdde8fe6ddac8b1970d89
Lennert Claeys <claeyslennert@gmail.com>
2023-11-11 18:17:52 +0100

    Add Playlist to allow downloading

---
+++ src/components/Playlist.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { BpList } from "../data/bplist";
+
+const { playlist } = defineProps<{
+  playlist: BpList;
+}>();
+</script>
+
+<template>
+  <a
+    class="playlist"
+    :download="`${playlist.playlistTitle}.bplist`"
+    :href="`data:text/json;charset=utf-8,${encodeURIComponent(
+      JSON.stringify(playlist)
+    )}`"
+  >
+    <img :src="playlist.image" />
+  </a>
+</template>
+
+<style scoped lang="scss">
+.playlist {
+  cursor: pointer;
+}
+</style>
